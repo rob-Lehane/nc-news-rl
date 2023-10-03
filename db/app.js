815d488d87f3cf6c49e7e7b5590e5a172f42c8b8
Rob Lehane <roblehane@Rob-Lehanes-MacBook-Pro.local>
2023-10-03 10:45:21 +0100

    task 4 completed with changes

--- db/app.js
+++ db/app.js
@@ -3,7 +3,7 @@ const app = express();
 const { getTopics } = require("./controllers/topics.controller")
 const { getApi } = require("./controllers/api.controller")
 const { getArticleById } = require("./controllers/articles.controller")
-const { handleCustomErrors, handle500Errors, handlePSQLErrors } = require('./controllers/errors.controller')
+const { handleCustomErrors, handle500Errors } = require('./controllers/errors.controller')
 
 app.use(express.json());
 
@@ -16,6 +16,5 @@ app.get("/api/articles/:article_id", getArticleById)
 
 app.use(handleCustomErrors)
 app.use(handle500Errors)
-app.use(handlePSQLErrors)
 
 module.exports = app;
--- db/controllers/errors.controller.js
+++ db/controllers/errors.controller.js
@@ -10,12 +10,3 @@ exports.handle500Errors = (err, req, res, next) => {
     console.log("500 error in console", err)
     res.status(500).send({ msg: 'internal server error' })
 }
-
-exports.handlePSQLErrors = (err, req, res, next) => {
-    const errorResponses = {
-        '22P02': { status: 400, msg: 'invalid data type' },
-    };
-
-    const errorResponse = errorResponses[err.code] || next(err);
-    res.status(errorResponse.status).send({ msg: errorResponse.msg });
-};
